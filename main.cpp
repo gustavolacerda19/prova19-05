6f2db0cd0765ed04b53e85564fe4d2eadbbb1124
Gustavo Lacerda <gustavolacerda655@gmail.com>
2020-05-19 08:13:13 -0300

    Leitura do volume

--- main.cpp
+++ main.cpp
@@ -4,8 +4,17 @@ using namespace std;
 
 int main()
 {
+    int compras;
+    double valormedio;
+
     cout << "SISTEMA DE PERFIL DE CLIENTE" << endl;
     cout << "--------------------------------" << endl;
     cout << "INFORME OS DADOS DO ULTIMO ANO" << endl;
+
+    cout << endl << "Quantas compras o cliente fez no ultimo ano? ";
+    cin >> compras;
+    cout << "Qual o ticket medio? ";
+    cin >> valormedio;
+
     return 0;
 }
